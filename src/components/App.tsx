528958d98a5d0f56478a7dd253748c8d2ee98bec
nahom-habtamu <nahomhab2626@gmail.com>
2021-10-27 12:14:06 +0300

    Remove Unwanted Code in App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,27 +1,10 @@
 const App = () => {
     return (
         <div>
-            <Child color="red"/>
-            <Child color="blue"/>
-            <Child color="yellow"/>
+            HELLO WORLD
         </div>
     );
 };
-
-interface ChildProps {
-    color : string;
-}
-
-
-const Child = ({ color } : ChildProps) => {
-    return (
-        <div>
-            { color }
-        </div>
-    );
-}
-
-
 
 
 export default App;
